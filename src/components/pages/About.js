3afba10c7570a291fbc0e03393606daefb9cdda1
bishal shrestha <biscro7@gmail.com>
2021-06-26 16:55:00 +0545

    setup Router

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React,{Fragment} from 'react'
+
+export const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>This is App to search Github users</p>
+      <p>Version:2.0.0</p>
+    </Fragment>
+  )
+}
+ 
